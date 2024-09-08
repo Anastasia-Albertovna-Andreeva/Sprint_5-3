15f65cbeff761c8602b02d3e8a08b863db6047c4
Anastasia Andreeva <stasya0111@gmail.com>
2024-09-08 18:01:33 +0200

    changes

--- urls.py
+++
@@ -1,6 +0,0 @@
-class URL:
-    home = 'https://stellarburgers.nomoreparties.site/'  # Домашняя страница
-    login = 'https://stellarburgers.nomoreparties.site/login'  # Страница входа
-    signup = 'https://stellarburgers.nomoreparties.site/register'  # Страница регистрации
-    password_reset = 'https://stellarburgers.nomoreparties.site/forgot-password'  # Восстановление пароля
-    user_profile = 'https://stellarburgers.nomoreparties.site/account/profile'  # Профиль пользователя
